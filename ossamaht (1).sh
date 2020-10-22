37fba2c614ea6ef35e6b8fb2c04fbca3edf5fe38
obrh1 <50054732+obrh1@users.noreply.github.com>
2020-10-22 18:16:11 +0100

    Me

---
+++ ossamaht (1).sh
@@ -0,0 +1,13 @@
+echo""
+echo""
+echo" WELCOME TO MY NEW SCRIPT"
+echo" CREAD BY OBRH" 
+cd 
+git clone https://github.com/SilentGhostX/HT-WPS-Breaker.git
+unzip HT-WPS-Breaker.zip
+cd HT-WPS-Breaker
+chmod +x HT-WB.sh
+./HT-WB.sh 
+echo" GOOD LUCK "
+echo" YARBI"
+exit
