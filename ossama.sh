39fe63720a50c83b56e8ad8b9b876c6d34d00418
obrh1 <50054732+obrh1@users.noreply.github.com>
2020-10-22 17:49:37 +0100

    Bom

---
+++ ossama.sh
@@ -0,0 +1,60 @@
+echo ""
+echo ""
+echo ""
+echo "The script is  made by AD TECH"
+echo ""
+echo ""
+echo ""
+echo "subscribe  our channel"
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+yum update -y                    
+yum install python -y           
+yum install python2 -y
+yum install ruby -y 
+yum install git -y
+yum install screenfetch -y
+yum install cowsay -y
+yum install toilet -y
+yum install figlet -y
+yum install php -y 
+yum install perl -y
+yum install nmap -y
+yum install openssh -y
+yum install bash -y
+yum install clang -y
+yum install jq -y
+yum install nano -y
+yum install curl -y
+yum install tar -y
+yum install zip -y
+yum install fish -y
+yum install unzip -y
+yum install tor -y 
+yum install sudo -y
+yum install wget -y
+yum install tor -y
+yum install w3m -y
+yum install cmatrix -y
+yum install wcalc -y 
+yum install openssl -y
+yum install bmon -y
+yum upgrade -y
+echo "ma3ndi mangol "
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "cread by ossama brh"
+echo ""
+echo ""
+exit
